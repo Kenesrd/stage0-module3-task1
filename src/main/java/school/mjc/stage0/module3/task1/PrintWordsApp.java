ce6f966a65f108a029d394ffbcf37b22660067e5
kenes <kenesvip@gmail.com>
2023-08-15 19:07:50 +0500

    module 3 done task 1

--- src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
+++ src/main/java/school/mjc/stage0/module3/task1/PrintWordsApp.java
@@ -2,8 +2,6 @@ package school.mjc.stage0.module3.task1;
 
 public class PrintWordsApp {
     public static void main(String[] args) {
-        System.out.println("Java");
-        System.out.println("is");
-        System.out.println("smth");
+        System.out.println("java" +'\n' +"is" + '\n' + "smth");
     }
 }
